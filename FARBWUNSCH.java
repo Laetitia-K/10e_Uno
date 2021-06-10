22b2f70983241549e422723196fbe265332ec939
Paula <paulazimmer42@gmil.com>
2021-06-10 11:09:28 +0200

    002

--- FARBWUNSCH.java
+++ FARBWUNSCH.java
@@ -1,33 +1,5 @@
-
-/**
- * Beschreiben Sie hier die Klasse FARBWUNSCH.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class FARBWUNSCH
+class FARBWUNSCH extends KARTEN
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse FARBWUNSCH
-     */
-    public FARBWUNSCH()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    
+    
 }
--- ZWEIZIEHEN.java
+++ ZWEIZIEHEN.java
@@ -1,33 +1,5 @@
-
-/**
- * Beschreiben Sie hier die Klasse ZWEIZIEHEN.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class ZWEIZIEHEN
+class ZWEIZIEHEN extends KARTEN
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse ZWEIZIEHEN
-     */
-    public ZWEIZIEHEN()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    
+    
 }
