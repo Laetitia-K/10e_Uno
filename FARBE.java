de4d9e7e1e7a17b7db25995382f63755a8c5d315
DanielKeber <keberdaniel0@gmail.com>
2021-06-10 11:20:20 +0200

    004

--- FARBE.java
+++ FARBE.java
@@ -2,4 +2,19 @@
 
 class FARBE
 {
+    FARBE rot;
+    FARBE gelb;
+    FARBE gruen;
+    FARBE blau;
+    
+    FARBE()
+    {
+        rot = new FARBE();
+        gruen = new FARBE();
+        blau = new FARBE();
+        gelb = new FARBE();
+        
+    }
+    
+    
 }
