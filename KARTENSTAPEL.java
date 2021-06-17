8f264ee658ffb8a92068332cdc3c9892354bfd01
DanielKeber <keberdaniel0@gmail.com>
2021-06-17 11:19:44 +0200

    Daniel17.06  fertig

--- KARTENSTAPEL.java
+++ KARTENSTAPEL.java
@@ -1,21 +1,27 @@
 
  class KARTENSTAPEL
 {
-    KARTENSTAPEL kartenstock;
+    KARTENSTAPEL ziehstapel;
     KARTENSTAPEL ablegestapel;
-    KARTENSTAPEL spielerhand1;
-    KARTENSTAPEL spielerhand2;
-    KARTENSTAPEL spielerhand3;
-    KARTENSTAPEL spielerhand4;
     
     
     KARTENSTAPEL()
     {
-        kartenstock  = new KARTENSTAPEL();
-        ablegestapel = new KARTENSTAPEL();
-        spielerhand1 = new KARTENSTAPEL();
-        spielerhand2 = new KARTENSTAPEL();
-        spielerhand3 = new KARTENSTAPEL();
-        spielerhand4 = new KARTENSTAPEL();
+         ziehstapel  = new KARTENSTAPEL();
+         ablegestapel = new KARTENSTAPEL();
+      
     }   
+    
+    void ziehstapel()
+    {
+        
+        
+    }
+    
+    void ablagestapel()
+    {
+        
+         
+    }
 }
+
